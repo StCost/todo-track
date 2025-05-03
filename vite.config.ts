fafced33cdee384d42ffe71f1b7674682803ec38
StCost <kostya010511@gmail.com>
2025-05-03 14:19:24 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set base to relative path for GitHub Pages
   server: {
     port: 3000,
     strictPort: true // Force the specified port, fail if not available
   },
   build: {
-    outDir: 'docs'
+    outDir: 'dist' // Keep dist as outDir to match with gh-pages deploy script
   }
 }) 
